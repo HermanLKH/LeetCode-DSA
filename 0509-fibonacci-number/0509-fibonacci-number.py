9b00ccfa19300ab9290543fcb6979c4cd95da368
Herman King How LING <57675016+HermanLKH@users.noreply.github.com>
2026-09-16 16:27:29 +0800

    Time: 49 ms (53.85%), Space: 19.2 MB (19.49%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -6,7 +6,7 @@ class Solution:
         first_num = 0
         sec_num   = 1
         
-        for _ in range(1, n, 1):
+        for _ in range(1, n):
             first_num, sec_num = sec_num, first_num + sec_num
 
         return sec_num
