e9beeb6163ec8b18fa052b838c3fdd44d9d71481
Herman King How LING <57675016+HermanLKH@users.noreply.github.com>
2026-09-19 09:07:35 +0800

    Time: 9 ms (40.82%), Space: 23.4 MB (8.48%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = ''.join(c.lower() for c in s if c.isalnum())
+
+        if len(s) == 0:
+            return True
+        
+        left = 0
+        right = -1
+
+        for _ in range(int(len(s) / 2)):
+            if s[left] == s[right]:
+                left, right = left + 1, right - 1
+            else:
+                return False
+
+        return True
