15bb8069fd4d021a8693aa7e38c900bcbe587085
Herman King How LING <57675016+HermanLKH@users.noreply.github.com>
2026-09-19 22:22:57 +0800

    Time: 7 ms (41.19%), Space: 19.2 MB (89.73%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,24 +1,19 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman_to_int = {
+        roman_numerals = {
             'I': 1,   'V': 5, 
             'X': 10,  'L': 50, 
             'C': 100, 'D': 500, 
             'M': 1000
         }
         num  = 0
-        fast = 1
 
-        for slow in range(len(s)):
-            if fast - slow == 1:
-                curr_val = roman_to_int[s[slow]]
-                next_val = roman_to_int[s[fast]] if fast < len(s) else None
-                
-                if next_val and curr_val < next_val:
-                    num += curr_val * -1 + next_val
-                    fast += 2
-                else:
-                    num += curr_val
-                    fast += 1
+        for i, rc in enumerate(s):
+            curr = roman_numerals[rc]
+
+            if i + 1 < len(s) and curr < roman_numerals[s[i + 1]]:
+                num -= curr
+            else:
+                num += curr
 
         return num
