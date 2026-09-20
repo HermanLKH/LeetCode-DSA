157583965000a0729e67e3f6b06a5ad6aa91918b
Herman King How LING <57675016+HermanLKH@users.noreply.github.com>
2026-09-20 18:53:10 +0800

    Time: 455 ms (95.8%), Space: 22.5 MB (18.78%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,22 +1,25 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
-        nums = sorted(nums)
+        nums.sort()
         l = 0
-
-        while len(nums) - l >= 3 and nums[l] + nums[-1] + nums[-2] < 0:
-            l += 1
 
         while len(nums) >= 3 and nums[-1] + nums[0] + nums[1] > 0:
             nums.pop()
+
+        while len(nums) - l >= 3 and nums[l] + nums[-1] + nums[-2] < 0:
+            l += 1
 
         if len(nums) - l < 3:
             return []
 
         size = len(nums)
         r = size - 1
-        res  = {}
+        res  = []
 
-        while l < size - 2:
+        for l in range(size - 2):
+            if l > 0 and nums[l] == nums[l - 1]:
+                continue
+
             m = l + 1
             r = size - 1
 
@@ -24,15 +27,18 @@ class Solution:
                 val = nums[l] + nums[m] + nums[r]
                 
                 if val == 0:
-                    tri = [nums[l], nums[m], nums[r]]
-                    key = (nums[l], nums[m], nums[r])
-                    res[key] = tri
+                    res.append([nums[l], nums[m], nums[r]])
                     m += 1
                     r -= 1
+
+                    while m < r and nums[m] == nums[m - 1]:
+                        m += 1
+
+                    while m < r and nums[r] == nums[r + 1]:
+                        r -= 1
                 elif val < 0:
                     m += 1
                 else:
                     r -= 1
-            l += 1
-
-        return list(res.values())
+                
+        return res
